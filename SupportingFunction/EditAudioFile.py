dcd955c288caf61f40b544bfc0ccf358df8b3ca5
Muhlik <1045321156@qq.com>
2024-11-09 12:50:05 +0800

    Unified playsound

--- SupportingFunction/EditAudioFile.py
+++ SupportingFunction/EditAudioFile.py
@@ -7,38 +7,13 @@ def multiOsRm(filepath):
         os.remove('./'+filepath)
     elif "linux" in platform.platform():
         os.remove('./'+filepath)
-    elif "windows" in platform.platform():
+    elif "Windows" in platform.platform():
         os.remove('./'+filepath)
-
-
-def windowsPlaysound(filepath, delete=True):
-    import winsound
-    winsound.PlaySound(filepath)    
-    if delete:
-        multiOsRm(filepath)    
-    # os.system("del  "+filepath)
-
-def macPlaysound(filepath, delete=True):
-    playsound(filepath)
-    if delete:
-        multiOsRm(filepath)   
-
-def LinuxPlaysound(filepath,delete=True):
-    playsound(filepath)
-    if delete:
-        multiOsRm(filepath)   
-
+ 
 def multiOsSound(filepath,delete=True):
-    if "mac" in platform.platform():
-        macPlaysound(filepath,delete)
-    elif "linux" in platform.platform():
-        LinuxPlaysound(filepath,delete)
-        print(platform.platform())
-    elif "windows" in platform.platform():
-        windowsPlaysound(filepath,delete)
-        print(platform.platform())
-    else:
-        print(platform.platform())
+    playsound(filepath)
+    if delete:
+        multiOsRm(filepath)
 
 
 
